0decce8aba407560594f2d9237707fcb7d913cf7
Ozzy Paniagua <ozzy.coding@gmail.com>
2018-04-29 15:11:55 -0700

    Add Video Code"

--- frontend/components/analysisPage/image_cropper.jsx
+++ frontend/components/analysisPage/image_cropper.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+// This code contains the end-user file picker and the image cropper
+// It needs to grab video files for the user
+// It needs to Slice the video into frames, then extract 10 frames from each quartile
+// Crop each frame, before updating state with the cropped 40 frames
+//This is where this component's role ends
+// Have the user pick 20 frames that contain faces
+// Pass the chosen to the model for evaluation
 class ImageCropper extends React.Component {
   constructor(props) {
     super(props);
     this.imgElement = null;
     this.inputElement = null;
     this.mat = null;
+    //the.path = null;
     this.createSrc = this.createSrc.bind(this);
     this.crop = this.crop.bind(this);
   }
@@ -23,6 +30,52 @@ class ImageCropper extends React.Component {
 
   onOpenCvReady() {
     document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+  }
+
+  getVideoImage(path, secs, callback) {
+    var me = this, video = document.createElement('video');
+    video.onloadedmetadata = function() {
+      if ('function' === typeof secs) {
+        secs = secs(this.duration);
+      }
+      this.currentTime = Math.min(Math.max(0, (secs < 0 ? this.duration : 0) + secs), this.duration);
+    };
+    video.onseeked = function(e) {
+      var canvas = document.createElement('canvas');
+      canvas.height = video.videoHeight;
+      canvas.width = video.videoWidth;
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      var img = new Image();
+      img.src = canvas.toDataURL();
+      callback.call(me, img, this.currentTime, e);
+    };
+    video.onerror = function(e) {
+      callback.call(me, undefined, undefined, e);
+    };
+    video.src = path;
+  }
+
+  showImageAt(secs) {
+    var duration;
+    this.getVideoImage(
+      '/html/mov_bbb.mp4',
+      function(totalTime) {
+        duration = totalTime;
+        return secs;
+      },
+      function(img, secs, event) {
+        if (event.type == 'seeked') {
+          var li = document.createElement('li');
+          li.innerHTML += '<b>Frame at second ' + secs + ':</b><br />';
+          li.appendChild(img);
+          document.getElementById('olFrames').appendChild(li);
+          if (duration >= ++secs) {
+            this.showImageAt(secs);
+          }
+        }
+      }
+    );
   }
 
   createSrc(e) {
