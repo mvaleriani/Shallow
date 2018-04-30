198142fe295523ea937e57864620e24eb7655e1c
mvaleriani <michaelvaleriani@gmail.com>
2018-04-29 18:36:59 -0700

    fix jank merge

--- frontend/components/analysisPage/AnalysisPage.jsx
+++ frontend/components/analysisPage/AnalysisPage.jsx
@@ -16,67 +16,25 @@ class AnalysisPage extends React.Component{
     this.currentTime = 0;
     this.duration = 0;
     this.onDrop = this.onDrop.bind(this);
-  }
+    }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log('Component Did Update');
-    console.log('prevProps',prevProps);
-    console.log('prevState',prevState);
-    if (this.state.vidFile) {
-      console.log("IF", this.state.vidFile);
-      // call showImageAt at the 4 quartiles
-      this.showImageAt(0);
-
-    componentDidUpdate() {
+    componentDidUpdate(prevProps, prevState) {
+        console.log('Component Did Update');
+        console.log('prevProps',prevProps);
+        console.log('prevState',prevState);
         if (this.state.vidFile) {
-        // call showImageAt at the 4 quartiles
+            console.log("IF", this.state.vidFile);
+            // call showImageAt at the 4 quartiles
+            this.showImageAt(0);
         }
     }
-  }
 
-  getVideoImage(path, secs, callback) {
-    var me = this, video = document.createElement('video');
+    getVideoImage(path, secs, callback) {
+        var me = this, video = document.createElement('video');
 
-    video.onloadedmetadata = function() {
-      if ('function' === typeof secs) {
-        secs = secs(this.duration);
-      }
-      this.currentTime = Math.min(Math.max(0, (secs < 0 ? this.duration : 0) + secs), this.duration);
-    };
-
-    video.onseeked = function(e) {
-      var canvas = document.createElement('canvas');
-      canvas.height = video.videoHeight;
-      canvas.width = video.videoWidth;
-      var ctx = canvas.getContext('2d');
-      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-      var img = new Image();
-      img.src = canvas.toDataURL();
-      callback.call(me, img, this.currentTime, e);
-    };
-
-    video.onerror = function(e) {
-      callback.call(me, undefined, undefined, e);
-    };
-
-    video.src = path;
-    this.duration = video.duration;
-    console.log("Video: ", video);
-    console.log("Video-Duration: ", this.duration);
-  }
-
-
-  showImageAt(secs) {
-    this.getVideoImage(
-      this.state.vidPath,
-      function(totalTime) {
-        // this.duration = totalTime;
-        return secs;
-      },
-      function(img, secs, event) {
-        if (event.type == 'seeked') {
-          this.crop(img);
-          }
+        video.onloadedmetadata = function() {
+        if ('function' === typeof secs) {
+            secs = secs(this.duration);
         }
         this.currentTime = Math.min(Math.max(0, (secs < 0 ? this.duration : 0) + secs), this.duration);
         };
@@ -98,8 +56,12 @@ class AnalysisPage extends React.Component{
 
         video.src = path;
         this.duration = video.duration;
+        console.log("Video: ", video);
+        console.log("Video-Duration: ", this.duration);
     }
 
+
+  
 
     showImageAt(secs) {
         this.getVideoImage(
