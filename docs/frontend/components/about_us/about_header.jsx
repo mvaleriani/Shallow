aac25806776847f09e8945d2a66ece99d8f924c9
mvaleriani <michaelvaleriani@gmail.com>
2018-05-22 10:00:27 -0700

    fix dev background

--- docs/frontend/components/about_us/about_header.jsx
+++ docs/frontend/components/about_us/about_header.jsx
@@ -4,10 +4,9 @@ class AboutHeader extends React.Component {
   render() {
     return (
         <section className="header-5 flex flex-align-center o-hidden p-x-20 min-h-350" data-cu-height="45%" style={{ backgroundImage:"url(https://i.imgur.com/glqKuG1.jpg)", minHeight:"648px", position:"relative"}}>
-          
+            
             <div id="nav-shadow"></div>
-          <video className="video-bg" src="../../app/assets/videos/video2.mp4" autoPlay loop ></video>
-            {/* <img id="dev-cover" src="https://github.com/mvaleriani/Shallow/blob/master/docs/app/assets/images/MichaelCoding.png?raw=true" className="cover" style={{width: "100%"}}/> */}
+          
           <div className="color7" style={{position: "relative"}}>
              
               <h1 id="dev-title" className="f-extra-light t-60 l-h-100 t-center cu-tweenmax"
